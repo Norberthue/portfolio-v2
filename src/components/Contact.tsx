75122f3ed5a70dc5bc5d89dace52da93aa70fc41
Norberthue <kokanorbert9@gmail.com>
2025-04-15 20:31:30 +0200

    fix problems for deploy

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,5 @@
 import { useInView } from 'framer-motion';
-import React, { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import Reveal from './utils/Reveal';
 
 interface ContactProps {
