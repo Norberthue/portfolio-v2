8e0e87bcdf59129cbe012c5b0fb04abab424da5c
Norberthue <kokanorbert9@gmail.com>
2025-04-29 12:45:54 +0200

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react"
-import Resume from "./components/Resume"
-import { Routes, Route} from 'react-router-dom'
 import MainPage from "./components/MainPage"
 
 function App() {
@@ -8,10 +6,7 @@ function App() {
   
   return (
       <div>
-        <Routes>
-          <Route path="/" element={<MainPage sectionActive={sectionActive} setSectionActive={setSectionActive}></MainPage>}></Route>
-          <Route path="/resume" element={<Resume/>} />
-        </Routes>
+          <MainPage sectionActive={sectionActive} setSectionActive={setSectionActive}></MainPage>
       </div>
   )
 }
--- src/components/Resume.tsx
+++
@@ -1,9 +0,0 @@
-const Resume = () => {
-  return (
-    <div>
-        <iframe src="./images/ResumeENG.pdf" className="w-screen h-screen"/>
-    </div>
-  )
-}
-
-export default Resume
